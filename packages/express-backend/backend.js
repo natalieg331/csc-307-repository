b161034db9d47defc32eb1d011a37931a26c4c1e
nataliegarcia <ngarciaq@calpoly.edu>
2025-10-06 13:59:23 -0700

    Getting users by ID using find()

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,20 +44,16 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
-app.get("/users", (req, res) => {
-  const name = req.query.name;
-  if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
   } else {
-    res.send(users);
+    res.send(result);
   }
 });
 
